69e322c5a90c3239ed9b18e7ec2055da5d7b524c
janlew <janlew97@gmail.com>
2021-02-08 18:34:18 +0100

    implemented experience amazing section carousel in jquery

--- app.js
+++ app.js
@@ -1,3 +1,76 @@
 $(document).ready(function () {
     $('.carousel').carousel('pause');
+
+    $('.amazing-slide').css({
+        "transform": "translateX(-100%)"
+    });
+
+    const $slides = $('.amazing-slide');
+    const size = 4;
+    const $prev = $('#amazing-prev');
+    const $next = $('#amazing-next');
+
+    let counter = 1;
+
+    $next.click(() => {
+        if (counter >= size - 1) return;
+        counter++;
+        $('.amazing-slide').css({
+            "transform": "translateX(" + (-100 * counter) + "%)",
+            "transition": "1s"
+        });
+    });
+
+    $prev.click(() => {
+        if (counter <= 0) return;
+        counter--
+        $('.amazing-slide').css({
+            "transform": "translateX(" + (-100 * counter) + "%)",
+            "transition": "1s"
+        });
+    });
+
+    $('.amazing-slide').on("transitionend", () => {
+        if ($slides[counter].id === "lastClone" ) {
+            counter = size - 2;
+            $('.amazing-slide').css({
+                "transform": "translateX(" + (-100 * counter) + "%)",
+                "transition": "none"
+            });
+        }
+
+        if ($slides[counter].id === "firstClone" ) {
+            counter = size - counter;
+            $('.amazing-slide').css({
+                "transform": "translateX(" + (-100 * counter) + "%)",
+                "transition": "none"
+            });
+        }
+    });
+
+    
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
